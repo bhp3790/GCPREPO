45e6db95b236907f1c97885619581839e47f6ea5
bhp3790 <52520866+bhp3790@users.noreply.github.com>
2019-08-16 12:42:54 +0530

    Update app.js
    Updated app.js file

--- app.js
+++ app.js
@@ -1,58 +1,50 @@
-var express= require('express');
-var app=express();
-var DataStore=require('@google-cloud/datastore');
+var express = require('express');
+var app = express();
+var DataStore = require('@google-cloud/datastore');
 var bodyParser = require('body-parser')
-var config=require('./config');
 app.use(bodyParser.urlencoded({ extended: false }))
+const projectId = 'sturdy-sentry-243203';
+const keyFile = './key.json';
+const kind = 'customer';
+
+// parse application/json
 app.use(bodyParser.json())
 
-const datastore= new DataStore({
-projectId:config.projectId,
-keyFile:config.keyFile
+const datastore = new DataStore({
+    projectId: projectId,
+    keyFile: keyFile
 });
 
-app.get('/getCustomers',(req,res)=>{
-var query=datastore.createQuery('customer');
-datastore.runQuery(query,(err,data)=>{
-    if(err)
-    console.log(err);
+app.get('/getCustomers', async (req, res) => {
+    var query = datastore.createQuery(kind);
+    var result = await datastore.runQuery(query);
+    if (!result[0])
+        res.send('data not found')
     else
-    res.send(data);
+        res.send(result[0]);
 })
-});
-app.get('/getCustomer',(req,res)=>{
+app.get('/getCustomer', async (req, res) => {
     console.log(req.query);
-    const id=req.query.id;
-    const query = datastore.createQuery('customer').filter('__key__', '=', datastore.key(['customer',id]));
-    datastore.runQuery(query,(err,data)=>{
-    if(err)
-    console.log(err);
+    const id = req.query.id;
+    const query = datastore.createQuery(kind).filter('__key__', '=', datastore.key([kind, id]));
+    var result = await datastore.runQuery(query);
+    if (!result[0])
+        res.send('data not found')
     else
-    res.send(data);
-
-    })
+        res.send(result[0]);
 })
-
-app.post('/addCustomer',(req,res)=>{
-    var key=datastore.key(['customer',req.body.id]);
-    var entity={
-        key:key,
-        data:{
-            FIRST_NAME:req.body.FIRST_NAME,
-            LAST_NAME:req.body.LAST_NAME,
-            EMAIL:req.body.EMAIL
-        }
+app.post('/addCustomer', async (req, res) => {
+    var key = datastore.key([kind, req.body.id]);
+    var entity = {
+        key: key,
+        data: req.body
     }
-    datastore.save(entity,(err,data)=>{
-        if(err)
-        res.status(404).send(err);
-        else
-        res.status(200).send('Customer details has  added successfully');
+    await datastore.save(entity);
+    res.status(200).send('data added successfully');
 
-    });
+});
 
-})
-const PORT=process.env.PORT || 3000;
-app.listen(PORT,(req,res)=>{
-    console.log(`Application started at port ${PORT}`)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (req, res) => {
+    console.log(`app started at port ${PORT}`)
 })
